af2a07031d8bef07ae35974b5d6005435426a11c
quanpower <quanpower@gmail.com>
2015-04-05 18:34:06 +0800

    add cv_drawing

---
+++ src/cv_drawing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+#Created by 'quanpower' on '15-4-5'
+#Email:quanpower@gmail.com
+#QQ:252527676
+#Site:www.smartlinkcloud.com
+
+__author__ = 'quanpower'
+
+import cv2
+import numpy as np
+
+img = np.zeros((512,512,3),np.uint8)
+
+cv2.line(img,(0,0),(511,511),(255,0,0),5)
+cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+cv2.circle(img,(447,63),63,(0,0,255),-1)
+cv2.ellipse(img,(256,256),(100,50),0,0,180,(255,0,255),-1)
+pts = np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
+pts = pts.reshape((-1,1,2))
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'smartlinkcloud',(10,500),font,4,(255,255,255),2)
+
+winname = 'example'
+cv2.namedWindow(winname)
+cv2.imshow(winname,img)
+cv2.waitKey(0)
+cv2.destroyWindow(winname)
---
+++ src/cv_numpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+#Created by 'quanpower' on '15-4-5'
+#Email:quanpower@gmail.com
+#QQ:252527676
+#Site:www.smartlinkcloud.com
+
+__author__ = 'quanpower'
+
+from numpy import *
+
+a = arange(15).reshape(3,5)
+print a
