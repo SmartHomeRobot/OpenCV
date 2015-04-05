39763b3b4fa6d4a9b8e17ddfbdb7eabd8cc4942b
quanpower <quanpower@gmail.com>
2015-04-05 21:27:27 +0800

    edit cv_mouse_drawing

--- src/cv_mouse_drawing.py
+++ src/cv_mouse_drawing.py
@@ -13,10 +13,26 @@ import cv2
 
 #events = [i for i in dir(cv2) if 'EVENT' in i]
 #print events
+drawing = False
+mode = True
+ix,iy = -1,-1
 
 def draw_circle(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img,(x,y),100,(255,0,0),-1)
+    global ix,iy,drawing,mode
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+    elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
+        if drawing==True:
+            if mode==True:
+                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            else:
+                #cv2.circle(img,(x,y),3,(0,0,255),-1)
+                r = int(np.sqrt((x-ix)**2+(y-iy)**2))
+                cv2.circle(img,(x,y),r,(0,0,255),-1)
+    elif event==cv2.EVENT_LBUTTONUP:
+        drawing=False
 
 img = np.zeros((512,512,3),np.uint8)
 cv2.namedWindow('image')
@@ -24,6 +40,10 @@ cv2.setMouseCallback('image',draw_circle)
 
 while(1):
     cv2.imshow('image',img)
-    if cv2.waitKey(20)&0xFF == 27:
+    k = cv2.waitKey(1)&0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k== 27:
         break
+
 cv2.destroyAllWindows()
